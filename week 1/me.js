4c1488ad0afa6d1325bf6866f1a57c55bcfafbbf
AlizeJackson <alize.jackson2018@gmail.com>
2025-10-21 20:52:31 -0500

    Add files via upload

---
+++ week 1/me.js
@@ -0,0 +1,8 @@
+let Name = 'Alize Jackson';
+let favoriteColor = 'Green';
+let hobby = 'playing video games';
+let favoriteFood= 'Meatloaf';
+let favoriteAnimal = 'Panda';
+
+console.log("Hello! I am " + Name + " and my favorite color is " + favoriteColor + ". My favorite animal is the " + 
+            favoriteAnimal + " and my favorite food is " + favoriteFood + ". My hobby is " + hobby + ".");
